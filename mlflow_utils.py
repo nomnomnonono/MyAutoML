f8d2cae6d136ee73a67f411edab969e5cefc3efb
nomnomnonono <murumuru1204@gmail.com>
2023-09-23 18:04:35 +0900

    fix bug of upload same experiment result to mlflow

--- mlflow_utils.py
+++ mlflow_utils.py
@@ -12,20 +12,30 @@ SERVICE_ACCOUNT_ID = os.environ.get("SERVICE_ACCOUNT_ID")
 TRACKING_URI = "http://127.0.0.1:5000/"
 
 
-def log_metric(expetiment_name, metadata):
+def log_metric(expetiment_name, metadata, params):
     mlflow.set_tracking_uri(TRACKING_URI)
     mlflow.set_experiment(expetiment_name.split("-")[0])
+
     with mlflow.start_run(run_name=expetiment_name):
         for key, value in metadata.items():
             if key == "model_uri":
                 mlflow.log_param(key, "gs://" + value.lstrip("/gcs/"))
             else:
                 mlflow.log_metric(key, value)
+        
+        for key, value in params.items():
+            mlflow.log_param(key, value)
+    
+    with open(".mlflow_history.txt", "a") as f:
+        f.write(expetiment_name + "\n")
 
 
 def update_mlflow():
     client = storage.Client()
     bucket = client.bucket(ARTIFACT_BUCKET.lstrip("gs://"))
+
+    with open(".mlflow_history.txt", "r") as f:
+        history = f.read().split("\n")
 
     pipeline_blobs = bucket.list_blobs(prefix=SERVICE_ACCOUNT_ID + "/", delimiter="/")
     pipeline_dirs = []
@@ -34,13 +44,20 @@ def update_mlflow():
 
     for pipeline_dir in pipeline_dirs:
         blobs = bucket.list_blobs(prefix=pipeline_dir + "train")
+        metadata, params = None, None
         for blob in blobs:
             if os.path.split(blob.name)[-1] == "executor_output.json":
                 content = blob.download_as_bytes()
                 result = json.load(BytesIO(content))
                 metadata = result["artifacts"]["metrics"]["artifacts"][0]["metadata"]
-                log_metric(blob.name.split("/")[1], metadata)
-                break
+                experiment_name = blob.name.split("/")[1]
+            if os.path.split(blob.name)[-1] == "best_params.json":
+                content = blob.download_as_bytes()
+                result = json.load(BytesIO(content))
+                params = result
+
+        if metadata is not None and params is not None and experiment_name not in history:
+            log_metric(experiment_name, metadata, params)
 
 
 if __name__ == "__main__":
